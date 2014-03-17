ca3552ca87ac409cf0a6e879a33778fa4ca893ab
samoth1601 <samoth1601@gmail.com>
2014-03-17 12:26:46 +0100

    bare flytta litt

---
+++ Calendar/src/objects/Person.java
@@ -0,0 +1,22 @@
+package objects;
+
+import java.util.ArrayList;
+
+public class Person {
+
+	public String navn;
+	public int tlf;
+	public String epost;
+	public ArrayList<Person> andreKalendere;
+	//TODO legg til en kalender
+	
+	
+	public Person(String navn, String epost, int tlf) {
+		this.navn = navn;
+		this.epost = epost;
+		this.tlf = tlf;
+		andreKalendere = new ArrayList<Person>();
+		//TODO fiks personens kalender
+	}
+	
+}
