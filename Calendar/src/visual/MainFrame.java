de229bf87d7f0ed49f6e09c87889066dccf3f604
Kristian <huskrist@hotmail.com>
2014-03-19 10:05:27 +0100

    Up to date

--- Calendar/src/visual/MainFrame.java
+++ Calendar/src/visual/MainFrame.java
@@ -20,6 +20,7 @@ import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.WindowConstants;
 
+import calculations.NorCalendar;
 import objects.Appointment;
 import objects.Person;
 
@@ -28,6 +29,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -44,6 +46,7 @@ import java.awt.event.MouseListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainFrame extends JPanel {
 
@@ -51,6 +54,15 @@ public class MainFrame extends JPanel {
 
 	private static JFrame loginFrame; 
 
+	private static CalendarView calendarView;
+	
+	private static Listener listener;
+	
+	private static JPanel miniCalendar;
+	private static NorCalendar miniNorCalendar;
+	private static JLabel miniCalendarMonth;
+	private static Container miniCalendarDays;
+	
 	private static JPanel leftPanel;
 	private JPanel checkPanel;
 	private JScrollPane checkScrollPane;
@@ -94,11 +106,10 @@ public class MainFrame extends JPanel {
 		mainFrame.add(new MainFrame());
 		mainFrame.pack();
 		mainFrame.setLocationRelativeTo(null);
-		mainFrame.setVisible(false);
+		mainFrame.setResizable(false);
 
+		mainFrame.revalidate();
 
-
-		/* DETTA E EN KOMMENTAR */
 	}
 
 
@@ -118,82 +129,215 @@ public class MainFrame extends JPanel {
 
 	public MainFrame() {
 		initLoginViewAndFrame();
-		addMouseListener(new Listener());
 		setPreferredSize(new Dimension(800, 600));
 		setBackground(Color.white);
 		setVisible(true);
 		setLayout(new BorderLayout(0,1));
 
+		calendarView = new CalendarView();
 		add(createLeftWindow(), BorderLayout.WEST);
 		add(new CalendarView());
 	}
 
-
 	public JPanel createLeftWindow() {
-		leftPanel = new JPanel();
-		leftPanel.setBorder(BorderFactory.createRaisedBevelBorder());
-		SpringLayout springLayout = new SpringLayout();
-		leftPanel.setLayout(springLayout);
-		leftPanel.setBackground(Color.white);
+		listener = new Listener();
+		
+    	leftPanel = new JPanel();
+    	leftPanel.setBorder(BorderFactory.createRaisedBevelBorder());
+    	SpringLayout springLayout = new SpringLayout();
+    	leftPanel.setLayout(springLayout);
+    	leftPanel.setBackground(Color.white);
 
-		searchField = new visual.CustomJTextField(new JTextField(), "/SEARCH.png", "Search..");
-		searchField.addKeyListener(new Listener());
-		searchField.setActionCommand("search typer");
-		searchField.setPreferredSize(new Dimension(180, 30));
+    	searchField = new visual.CustomJTextField(new JTextField(), "/SEARCH.png", "Search..");
+    	searchField.addKeyListener(listener);
+    	searchField.setActionCommand("search typer");
+    	searchField.setPreferredSize(new Dimension(180, 30));
+    	
+    	Container container = new Container();
+    	container.setLayout(new GridBagLayout());
+    	GridBagConstraints c = new GridBagConstraints();
+    	c.gridx = 0; c.gridy = 0; c.ipadx = 20;
+    	
+    	JCheckBox cb1 = new JCheckBox();
+    	cb1.addActionListener(listener);
+    	cb1.setActionCommand("Andre kalendere");
+        JLabel cb1Description = new JLabel();
+        cb1Description.setText("Andre kalendere");
+        
+        JButton nyAvtaleBtn = new JButton("Ny avtale");
+        nyAvtaleBtn.setPreferredSize(new Dimension(235, 40));
+        
+        ImageIcon caret = new ImageIcon(this.getClass().getResource("/concat.png"));
+        JButton searchDropDown = new JButton("Søk etter bruker");
+        searchDropDown.setActionCommand("Search button");
+        searchDropDown.setPreferredSize(new Dimension(180, 30));
+        searchDropDown.addActionListener(listener);
+        searchDropDown.setIcon(caret);
+        searchDropDown.setHorizontalTextPosition(SwingConstants.LEFT);
+        leftPanel.add(searchDropDown);
+        
 
-		Container container = new Container();
-		container.setLayout(new GridBagLayout());
+        createMiniCalendar();
+        leftPanel.add(miniCalendar);
+        springLayout.putConstraint(SpringLayout.NORTH, miniCalendar, 30, SpringLayout.SOUTH, nyAvtaleBtn);
+        springLayout.putConstraint(SpringLayout.WEST, miniCalendar, 25, SpringLayout.WEST, leftPanel);
+        
+        createUserSearch();
+        
+        springLayout.putConstraint(SpringLayout.NORTH, searchDropDown, 40, SpringLayout.SOUTH, miniCalendar); // OK
+        springLayout.putConstraint(SpringLayout.NORTH, searchScrollPane, 0, SpringLayout.SOUTH, searchDropDown);
+        springLayout.putConstraint(SpringLayout.NORTH, container, 30, SpringLayout.SOUTH, searchDropDown); // OK
+
+        container.add(cb1Description, c);
+        c.gridx = 1;
+        container.add(cb1, c);
+
+    	leftPanel.add(nyAvtaleBtn);
+    	leftPanel.add(searchScrollPane);
+        leftPanel.add(container, SpringLayout.EAST);
+
+        searchLayout = new SpringLayout();
+        searchPanel.setLayout(searchLayout);
+        searchPanel.add(searchField);
+        searchLayout.putConstraint(SpringLayout.NORTH, searchField, 10, SpringLayout.NORTH, searchPanel);
+        
+        createPersonCheckPanel();
+        springLayout.putConstraint(SpringLayout.NORTH, checkScrollPane, 50, SpringLayout.SOUTH, container);        
+        leftPanel.add(checkScrollPane);
+        leftPanel.setPreferredSize(new Dimension(240, 800));
+    	leftPanel.setVisible(true);
+    	
+    	return leftPanel;
+    }
+
+	
+	public void createMiniCalendar() {
+		miniNorCalendar = new NorCalendar();
+				
+		miniCalendar = new JPanel();
+		miniCalendar.setBackground(Color.white);
+		miniCalendar.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0; c.gridy = 0; c.ipadx = 20;
-
-		JCheckBox cb1 = new JCheckBox();
-		cb1.addActionListener(new Listener());
-		cb1.setActionCommand("Andre kalendere");
-		JLabel cb1Description = new JLabel();
-		cb1Description.setText("Andre kalendere");
-
-		JButton nyAvtaleBtn = new JButton("Ny avtale");
-		nyAvtaleBtn.addActionListener(new Listener());
-		nyAvtaleBtn.setPreferredSize(new Dimension(235, 40));
-		nyAvtaleBtn.setActionCommand("New Appointment");
-
-		ImageIcon caret = new ImageIcon(this.getClass().getResource("/concat.png"));
-		JButton searchDropDown = new JButton("S�k etter bruker");
-		searchDropDown.setActionCommand("Search button");
-		searchDropDown.setPreferredSize(new Dimension(180, 30));
-		searchDropDown.addActionListener(new Listener());
-		searchDropDown.setIcon(caret);
-		searchDropDown.setHorizontalTextPosition(SwingConstants.LEFT);
-		leftPanel.add(searchDropDown);
-
-
-
-		createUserSearch();
-
-		springLayout.putConstraint(SpringLayout.NORTH, searchDropDown, 40, SpringLayout.SOUTH, nyAvtaleBtn); // OK
-		springLayout.putConstraint(SpringLayout.NORTH, container, 30, SpringLayout.SOUTH, searchDropDown); // OK
-		springLayout.putConstraint(SpringLayout.NORTH, searchScrollPane, 0, SpringLayout.SOUTH, searchDropDown);
-
-		container.add(cb1Description, c);
-		c.gridx = 1;
-		container.add(cb1, c);
-
-		leftPanel.add(nyAvtaleBtn);
-		leftPanel.add(searchScrollPane);
-		leftPanel.add(container, SpringLayout.EAST);
-
-		searchLayout = new SpringLayout();
-		searchPanel.setLayout(searchLayout);
-		searchPanel.add(searchField);
-		searchLayout.putConstraint(SpringLayout.NORTH, searchField, 10, SpringLayout.NORTH, searchPanel);
-
-		createPersonCheckPanel();
-		springLayout.putConstraint(SpringLayout.NORTH, checkScrollPane, 50, SpringLayout.SOUTH, container);        
-		leftPanel.add(checkScrollPane);
-		leftPanel.setPreferredSize(new Dimension(240, 800));
-		leftPanel.setVisible(true);
-
-		return leftPanel;
+		c.gridx = 0; c.gridy = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		Container labels = new Container();
+		labels.setLayout(new BorderLayout());
+		
+		JLabel left = new JLabel("<");
+		left.setFont(new Font(Font.MONOSPACED, Font.BOLD, 12));
+		left.setPreferredSize(new Dimension(20,12));
+		left.setBackground(new Color(215,150,0));
+		left.setBorder(BorderFactory.createRaisedBevelBorder());
+		left.addMouseListener(listener);
+		labels.add(left, BorderLayout.WEST);
+		
+		miniCalendarMonth = new JLabel();
+		int curMonth = miniNorCalendar.MONTH;
+		miniCalendarMonth.setText(miniNorCalendar.month(curMonth) + " - " + Integer.toString(miniNorCalendar.YEAR) );
+		miniCalendarMonth.setHorizontalAlignment(JLabel.CENTER);
+		labels.add(miniCalendarMonth, BorderLayout.CENTER);
+		
+		JLabel right = new JLabel(" >");
+		right.setBorder(BorderFactory.createRaisedBevelBorder());
+		right.setPreferredSize(new Dimension(20,12));
+		right.setBackground(new Color(215,150,0)); // TODO Velg en bra farge
+		right.setFont(new Font(Font.MONOSPACED, Font.BOLD, 12));
+		right.addMouseListener(listener);
+		labels.add(right, BorderLayout.EAST);
+		miniCalendar.add(labels, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		Container header = createHeader();
+		miniCalendar.add(header, c);
+		
+		c.gridy++;
+		miniCalendarDays = new Container();
+		miniCalendar.add(updateDaysOfMonth(), c);
+	}
+	
+	
+    public static Container updateDaysOfMonth() {
+    	GridBagConstraints c = new GridBagConstraints();
+    	c.gridx = 0; c.gridy = 0;
+    	
+    	miniCalendarMonth.setText(miniNorCalendar.month(miniNorCalendar.MONTH) + " - " + miniNorCalendar.YEAR);
+		miniCalendarDays.removeAll();
+		miniCalendarDays.setLayout(new GridBagLayout());
+		int firstDay = miniNorCalendar.getFirstDayOfMonth();
+		int lastDay = miniNorCalendar.getLastDayOfMonth();
+		
+		int lastDayLastWeekLastMonth = miniNorCalendar.getLastDayOfLastWeekInLastMonth();
+		
+		int nextMonthDayCounter = 1;
+		int dayCounter = 0;
+		for (int i = 0; i < 6; i++) {
+			JPanel week = new JPanel();
+			week.setBackground(Color.white);
+			week.setLayout(new GridBagLayout());
+			week.addMouseListener(listener);
+			week.setName(Integer.toString(i));
+			for (int j = 0; j< 7; j++) {
+				JPanel day = new JPanel();
+				
+				day.setBackground(Color.white);
+				day.setBorder(BorderFactory.createLineBorder(Color.black));
+				day.setVisible(true);
+				JLabel dateOfDay = new JLabel();
+				dateOfDay.setPreferredSize(new Dimension(14,14));
+				dateOfDay.setName("black");
+				dateOfDay.setVisible(true);
+				if ( (i == 0 && j >= firstDay) || (i > 0 && dayCounter < lastDay) ) {
+					dayCounter++;
+					if(dayCounter < 10) { // #SJÅFINTUT
+						dateOfDay.setText(Integer.toString(dayCounter));
+					}
+					else {
+						dateOfDay.setText(Integer.toString(dayCounter));
+					}
+				}
+				else if (j < firstDay && i < 2) {
+					dateOfDay.setText(Integer.toString(lastDayLastWeekLastMonth-firstDay+j+1));
+					dateOfDay.setName("gray");
+					dateOfDay.setForeground(Color.gray);
+				}
+				else {
+					dateOfDay.setText(" " + Integer.toString(nextMonthDayCounter));
+					dateOfDay.setName("gray");
+					dateOfDay.setForeground(Color.gray);
+					nextMonthDayCounter++;
+				}
+				day.add(dateOfDay);
+					week.add(day);
+			}
+			for(int empty = 0; empty < 7; empty++) {
+				if( ((JLabel) ((JPanel) week.getComponent(empty)).getComponent(0)).getText() != " " && nextMonthDayCounter < 8) {
+					miniCalendarDays.add(week, c);
+					c.gridy ++;		
+				}
+			}
+		}
+		miniCalendar.revalidate();
+		return miniCalendarDays;
+    }
+	
+	private Container createHeader() {
+		Container header = new Container();
+		header.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0; c.gridy = 0;
+		for(int d = 0; d < 7; d++) {
+			c.gridx++;
+			JPanel headerDayPanel = new JPanel();
+			headerDayPanel.setBackground(new Color(255,208,112));
+			headerDayPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+			JLabel dayDescription = new JLabel( Character.toString(CalendarView.days[d].toString().charAt(0)) ); // TODO GET THIS DATE
+			dayDescription.setPreferredSize(new Dimension(14,14));
+			headerDayPanel.add(dayDescription);
+			header.add(headerDayPanel);
+		}
+		return header;
 	}
 
 	public void createPersonCheckPanel() {
@@ -348,19 +492,81 @@ public class MainFrame extends JPanel {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			if (e.getSource() instanceof JLabel) {
+				if( ((JLabel) e.getSource()).getText() == "<") {
+					miniNorCalendar.monthBack();
+					miniCalendarDays = updateDaysOfMonth();
+				}
+				else {
+					miniNorCalendar.monthForward();
+					miniCalendarDays = updateDaysOfMonth();
+				}
+			}
+			if (e.getSource() instanceof JPanel) {
+				String name = ((JPanel) e.getSource()).getName();
+				int weekOfMonth = Integer.parseInt(name);
+				System.out.println(name);
+				calendarView.getCalendar().setTime(miniNorCalendar.getTime());
+				calendarView.getCalendar().setWeek(weekOfMonth);
+				updateDaysOfMonth();
+				calendarView.updateWeekDates();
+				System.out.println("weekdays: " + calendarView.getCalendar().getWeekDates()[0]);
+				System.out.println("source char: " + ((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(0)).getComponent(0)).getText());
+				try {
+				if(calendarView.getCalendar().getWeekDates()[0].charAt(0) != ((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(0)).getComponent(0)).getText().charAt(0)
+						|| (calendarView.getCalendar().getWeekDates()[0].charAt(1) != ((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(0)).getComponent(0)).getText().charAt(1) 
+							&& ((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(0)).getComponent(0)).getText().length() < 3 )) {
+					calendarView.getCalendar().nextWeek();
+					updateDaysOfMonth();
+					calendarView.updateWeekDates();
+				}
+				}
+				catch(IndexOutOfBoundsException exc) {
+					System.out.println("sum ting wong");
+				}
+//				if (weekOfMonth == 0) { // For whatever reason, this must be done twice. I suspect it is because of the Calendar interprets 0 as the last week in last month, instead of first this month
+//					updateDaysOfMonth();
+//					calendarView.updateWeekDates();
+//				}
+				calendarView.getCurrentWeek().setText("Uke " + calendarView.getCalendar().get(Calendar.WEEK_OF_YEAR));
+				
+			}
+			
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			if (e.getSource() instanceof JLabel) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			}
+			if (e.getSource() instanceof JPanel) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				for (int i = 0; i < ((JPanel) e.getSource()).getComponentCount(); i++) {
+					((JPanel)((JPanel) e.getSource()).getComponent(i)).setBackground(new Color(0,200,255));
+					((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(i)).getComponent(0)).setForeground(Color.white);
+				}
+			}
 		}
-
 		@Override
 		public void mouseExited(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			if (e.getSource() instanceof JLabel) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+			}
+			if (e.getSource() instanceof JPanel) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+				for (int i = 0; i < ((JPanel) e.getSource()).getComponentCount(); i++) {
+					((JPanel)((JPanel) e.getSource()).getComponent(i)).setBackground(Color.white);
+					if(	((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(i)).getComponent(0)).getName() == "black") {
+						((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(i)).getComponent(0)).setForeground(Color.black);
+					}
+					else {
+						((JLabel)((JPanel)((JPanel) e.getSource()).getComponent(i)).getComponent(0)).setForeground(Color.gray);
+					}
+				}
+			}
+			
 		}
 
 		@Override
@@ -369,12 +575,17 @@ public class MainFrame extends JPanel {
 			if(!searchScrollPane.isFocusOwner() && searchScrollPane.isVisible()) {
 				searchScrollPane.setVisible(false);
 			}
+			if (e.getSource() instanceof JLabel) {
+				((JLabel) e.getSource()).setBorder(BorderFactory.createLoweredBevelBorder());
+			}
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			if (e.getSource() instanceof JLabel) {
+				((JLabel) e.getSource()).setBorder(BorderFactory.createRaisedBevelBorder());
+			}
 		}
 
 		@Override
@@ -415,7 +626,13 @@ public class MainFrame extends JPanel {
 	}
 
 
-
+    public static CalendarView getCalendarView() {
+    	return calendarView;
+    }
+    
+    public static void setMiniCalendarDays(Container cont) {
+    	miniCalendarDays = cont;
+    }
 
 
 
